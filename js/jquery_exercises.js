ff618d84a31babaafb4b024b870c4e8baaabf0b3
Graham Davis <davis.graham.satx@gmail.com>
2018-06-29 16:59:33 -0500

    konami exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -3,31 +3,54 @@
 
 
 $(function() {
-    // alert($("#dogList").text());
-    // alert($("#newList").text());
+    // alert($("#dogList").text()); //creates an alert on page load
+    // alert($("#newList").text()); //creates an alert on page load
+
     $("#playButton").click(function() {
         $("#playOutput").text($("#playInput").val());
         $("h1").toggle(250)
     });
+
+    //changes background color of .importante
     $(".importante").hover(function(){
         $(this).css("background-color", "indianred");
     }, function(){
         $(this).css("background-color", "rgba(0,0,0,0)");
     });
+
+    //changes the text color of li elements on hover
+    $("li").hover(function(){
+        $(this).css("color", "indianred");
+    }, function(){
+        $(this).css("color", "rgba(0,0,0,1)");
+    });
+
+    //changes the background color of all h1 elements on dblclick
+    $('h1').dblclick(function(){
+        $(this).css('background-color', 'indianred');
+    });
+
+    //reverts background color change on all h1 elemments on single click
+    $('*').click(function(){
+        $(this).css('background-color', 'rgba(0,0,0,0');
+    });
+
+    //increases the font size of each p element when the element is dbl clicked
+    $('p').dblclick(function(){
+        $(this).css('font-size', '18px')
+    });
+
+    //returns the default font size of all p elements when the element is single clicked
+    $('p').click(function(){
+        $(this).css('font-size', '1em')
+    });
+
+    //logs keystrokes in the input field at top of page
+    $('#playInput').keyup(function(event){
+       console.log(event);
+    });
 });
 
-
-
-// Add jQuery code that will change the background color of a h1 element when clicked.
-//
-// Make all paragraphs have a font size of 18px when they are double clicked.
-//
-// Set all li text color to red when the mouse is hovering, reset to black when it is not.
-
-
-$('h1').dblclick(function () {
-    $(this).css('background-color', 'indianred');
-});
 
 
 
