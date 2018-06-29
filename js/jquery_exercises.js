1b88c67710953008895a47d0404b872ecd171872
Graham Davis <davis.graham.satx@gmail.com>
2018-06-29 14:04:57 -0500

    jquery selector exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,45 +1,43 @@
 "use strict";
 
-// //JS
-// window.onload = function() {
-//     document.getElementById("playButton").addEventListener("click", function() {
-//         var input = document.getElementById("playInput").value;
-//         console.log(input);
-//         document.getElementById("playOutput").innerText = input;
-//     });
-// };
 
-// //JQuery
-
-// $(document).ready(function() {
-//     alert( 'The DOM has finished loading!' );
-// });
 
 $(function() {
+    // alert($("#dogList").text());
+    // alert($("#newList").text());
     $("#playButton").click(function() {
         $("#playOutput").text($("#playInput").val());
-        $("h1").toggle(500)
+        $("h1").toggle(250)
     });
-    $(".importante").on("mouseover", function() {
-        $(this).css("background-color", "yellow")
+    $(".importante").hover(function(){
+        $(this).css("background-color", "indianred");
+    }, function(){
+        $(this).css("background-color", "rgba(0,0,0,0)");
     });
+});
+
+
+
+// Add jQuery code that will change the background color of a h1 element when clicked.
+//
+// Make all paragraphs have a font size of 18px when they are double clicked.
+//
+// Set all li text color to red when the mouse is hovering, reset to black when it is not.
+
+
+$('h1').dblclick(function () {
+    $(this).css('background-color', 'indianred');
 });
 
 
 
 
 
-
-
-
-
-
-
-
-$(document).ready(function()
-{
-    $('#default').BaconIpsum();
-    $('#custom').BaconIpsum({ type:'all-meat', paras:5, start_with_lorem:false });
-    $('#one-sentence').BaconIpsum({ type:'meat-and-filler', start_with_lorem:true, sentences:1 });
-    $('#no-tags').BaconIpsum({ type:'meat-and-filler', start_with_lorem:true, paras:1, no_tags: true });
-});
+// Bacon Lorem Ipsum
+// $(document).ready(function()
+// {
+//     $('#default').BaconIpsum();
+//     $('#custom').BaconIpsum({ type:'all-meat', paras:5, start_with_lorem:false });
+//     $('#one-sentence').BaconIpsum({ type:'meat-and-filler', start_with_lorem:true, sentences:1 });
+//     $('#no-tags').BaconIpsum({ type:'meat-and-filler', start_with_lorem:true, paras:1, no_tags: true });
+// });
