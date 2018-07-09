f2a2f0a8c2cd757b406b67d808242b960fe293c7
Graham Davis <davis.graham.satx@gmail.com>
2018-07-09 11:04:36 -0500

    second solution for exercise

--- js/pre-ajax.js
+++ js/pre-ajax.js
@@ -17,20 +17,38 @@ $(document).ready(function(){
         }
     ];
 
-    $('#add-names-btn').click(function () {
+    function buildList(arrObjs) {
+        var output = "";
+        output += "<ul>";
+        arrObjs.forEach(function(person){
+           output += "<li style='color:"+ person.favColor + "'>" + person.name + "</li>";
+        });
+        output += '</ul>';
+        return output;
+    }
 
-        if($('#names-list').html() === '') {
-            people.forEach(function (ele) {
-
-                $('#names-list').append('<li>' + ele.name + '</li>');
-
-            });
-
-            $('li').each(function (index) {
-                $(this).css('color', people[index].favColor);
-            })
-
-        }else{console.log('You Already Printed This Array!')};
+    $('#add-names-btn').click(function() {
+        $('#names-list').html(buildList(people));
     });
+
+
+
+
+
+    // $('#add-names-btn').click(function () {
+    //
+    //     if($('#names-list').html() === '') {
+    //         people.forEach(function (ele) {
+    //
+    //             $('#names-list').append('<li>' + ele.name + '</li>');
+    //
+    //         });
+    //
+    //         $('li').each(function (index) {
+    //             $(this).css('color', people[index].favColor);
+    //         })
+    //
+    //     }else{console.log('You Already Printed This Array!')};
+    // });
 
 });
